39f4e79ce937397ded84f247a609cbb7c7adc255
swdev.smz <swdev.smz@gmail.com>
2018-06-24 20:22:10 +0900

    commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,13 +9,6 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  /*
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  */
-
   heroes = HEROES;
 
   selectedHero: Hero;
