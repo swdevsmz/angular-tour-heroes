11ed57833bff586834289add192dd410c578e690
swdev.smz <swdev.smz@gmail.com>
2018-06-26 01:39:11 +0900

    commit

---
+++ src/app/hero.child.ts
@@ -0,0 +1,5 @@
+import { Hero } from './hero';
+
+export class HeroChild extends Hero {
+    company: string;
+}
--- src/app/heroes/heroes.child.component.ts
+++ src/app/heroes/heroes.child.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroChild } from '../hero.child';
+import { HeroesComponent } from './heroes.component';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -7,19 +9,21 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesChildComponent extends HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
-  protected selectedHero: Hero;
+  protected selectedHero: HeroChild;
 
-  constructor() { }
+  constructor() {
+      super();
+  }
 
   ngOnInit() {
   }
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    this.selectedHero = <HeroChild>hero;
   }
 
 }
