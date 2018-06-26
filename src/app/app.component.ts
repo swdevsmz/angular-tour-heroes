84b7b8f81ead9047e0752a97567091da43dc63a1
swdev.smz <swdev.smz@gmail.com>
2018-06-26 23:52:36 +0900

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heros';
+
+  p: Parent;
+
+  constructor() {
+    // this.p = new Parent();
+    // this.p.no = '10';
+  }
+
+  async ngOnInit () {
+
+    this.p = new Parent();
+    await this.getNo();
+  }
+  public async getNo(): Promise<string> {
+    return await this.p.no;
+  }
+  // p.no;
+}
+
+export class Parent {
+  no: string;
+  child: Child;
+}
+
+export class Child {
+  nama: string;
 }
